84c2695afb0626db19fa8cb3074e8b5deb656f9d
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-10-04 00:18:51 +0300

    update proxy with new endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
@@ -9,10 +9,10 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://backend-auth-cm3.onrender.com",
+        // target: "https://backend-auth-cm3.onrender.com",
+        target: "https://w7-cm-backend-no-auth.onrender.com",
         changeOrigin: true,
       },
     },
   },
 });
-
