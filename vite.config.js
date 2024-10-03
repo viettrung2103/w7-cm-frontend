991736ba66c2f73f7000407c3db5f85becc949ae
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-10-04 01:43:16 +0300

    edit config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    // port: 3000,
     proxy: {
       "/api": {
         // target: "https://backend-auth-cm3.onrender.com",
@@ -14,8 +14,8 @@ export default defineConfig({
         // target: "https://w7-cm-backend-no-auth.onrender.com",
         // AUTH
         target: "https://w7-cm-backend-auth.onrender.com",
-
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
     },
   },
